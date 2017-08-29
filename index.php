f601b549de5825268f5e766ef4bdd8e86a6fdb04
IngCristhian <cristhianplaza.o@gmail.com>
2017-08-28 21:25:14 -0500

    New Index

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">		
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    	<style type="text/css">
+    		#hover_effect_loggin {
+    			color: black;
+    			background-color: rgba(0,18,56,.1); 
+    			text-decoration: none;
+    			border-bottom: 3px solid green;
+    			border-radius: 5px; 
+    		}
+    		#hover_effect_loggin:hover {
+    			background-color: rgba(0,18,56,.3); 
+    			color:white;
+    			text-decoration: none;
+    			border-bottom: 3px solid green;
+    			border-radius: 5px; 
+    		}
+    	</style>
+
+	</head>
+	<body>
+		<div class="container-fluid">
+			<div class="row align-items-center py-2" style="border-bottom:3px solid lightgrey;">
+				<div class="col-md-10 hidden-sm-down">		
+					<a class="navbar-brand text-muted" href="#">Inneur Courses</a>
+				</div>
+				<div class="col-sm-12 hidden-md-up" align="center" style="border-bottom: 1px solid lightgrey; ">		
+					<a class="navbar-brand" href="#">Inneur Courses</a>
+				</div>
+				<div class="col-md-2 hidden-sm-down" align="right">
+					<a id="hover_effect_loggin" class="py-2 px-3" href="#">Inicia Sesión</a>
+				</div>
+				<div class="col-sm-12 hidden-md-up my-2" align="center">
+					<a class="py-1 px-5" id="hover_effect_loggin" href="#">Inicia Sesión</a>
+				</div>
+			</div>
+			<div class="row py-5 align-items-center" style="background: url('madera.png');">
+				<div class="col-md-3 offset-sm-2 hidden-sm-down">
+					<h4 align="center" style="color: lightgrey; text-shadow: 1px 1px 2px black;">Aprende con Inneur, estamos en periodo de prueba y en etapa de desarrollo</h4>
+				</div>
+				<div class="col-md-3 offset-md-2 px-3 py-4 col-6 offset-3" style="border-radius: 5px; border: 0.5px solid rgba(0,18,56,.5); background-color: rgba(0,18,56,.3);">
+					<p class="text-white" align="center" style="font-weight: 600;">Inicia Sesión</p>
+					<form class="row mx-2">
+						<input class="form-control py-2" type="email" name="" placeholder="Escribe tu correo">
+						<button type="button" class="btn btn-primary form-control py-1 my-2" name="">¡Aprende Ya!</button>
+					</form>
+				</div>	
+				
+			</div>
+		</div>
+
+		<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+	</body>
+</html>
